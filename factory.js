1d62311e98cb6457cc5bf86580d14c0d90bec9c7
Matthew Elliston <mpgelliston@gmail.com>
2019-10-26 17:29:15 +0100

    Added some more notes to the factory

--- factory.js
+++ factory.js
@@ -1,8 +1,14 @@
+/**
+ * Factory class is creation pattern,
+ * Manages the Creates objects, usually of a similar type.
+ * Different but similarities between the two would
+ */
 class BallFactory {
   createBall(type){
     let ball;
     if (type === 'football' || type === 'soccer') ball = new Football();
     else if (type === 'basketball') ball = new Basketball();
+    else throw `Ball Type "${type}" Not Found`;
     ball.roll = () =>{
       return `${ball._type} is rolling`;
     }
@@ -32,3 +38,15 @@ const ballfactory = new BallFactory();
 const myFootball = ballfactory.createBall('football');
 console.log(myFootball.kick());
 console.log(myFootball.roll());
+const myBasketball = ballfactory.createBall('basketball')
+console.log(myBasketball.roll());
+console.log(myBasketball.bounce());
+
+// Attempts a ball type that isnt there.
+try{
+  const golfBall = ballfactory.createBall('golf');
+}
+catch(e){
+  console.log(e);
+}
+
